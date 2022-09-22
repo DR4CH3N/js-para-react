935366f65679ff290843a195b67ae2c197e41ff1
Matheus Anthony <thehellc4t@gmail.com>
2022-09-22 11:48:31 -0300

    codigo modular
    aprendendo como fazer export, import e como criar modulos

---
+++ 04-codigo-modular.js
@@ -0,0 +1,5 @@
+import { pessoa, curso as treinamento } from "./modules/pessoa.js";
+
+console.log(pessoa.nome );
+console.log(treinamento.titulo );
+
---
+++ modules/pessoa.js
@@ -0,0 +1,15 @@
+ const pessoa = {
+    nome : 'matheus',
+    idade : 20,
+    cidade : 'são paulo'
+}
+
+
+ const curso = {
+    titulo : 'HTML e CSS',
+    cargaHoraria : 40
+}
+
+
+
+export { pessoa, curso }
